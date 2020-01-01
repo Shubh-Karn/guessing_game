df477dfda7dcc6681eb5dd008795ac969235be6c
Shubhendu Shubham <s2.shubh2@gmail.com>
2020-01-02 02:49:29 +0530

    Added a console warning when user gives input other than number when inputing guessed number.

--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,10 @@ fn main() {
 
         let guess: u32 = match guess.trim().parse() {
             Ok(num) => num,
-            Err(_) => continue,
+            Err(_) => {
+                println!("Please Type a number");
+                continue;
+            }
             //.expect("Plesae type a number");
         };
 
